8f0af9f2a5efc287bda6ed6135541ea984c0ccb3
Rafael Garcia <rafaelgarcia2013@gmail.com>
2018-05-09 18:01:53 -0300

    Código Sudoku

--- sudoku_main.cpp
+++ sudoku_main.cpp
@@ -1,68 +1,82 @@
 #include <iostream>
 #include <cstdlib>
+#include "console.h"
 #include "sudoku.h"
-#include "sudoku_form.h"
 
 using namespace std;
 
 int main(void)
 {
-  Sudoku Origem("sudoku.txt"),S(Origem);
+  Sudoku Origem,S;
   Jogada J;
 
-  exibir_cabecalho();
+  if (!Term.formatado())
+  {
+    cout << "Este terminal nao permite saida formatada! Executanto em modo simples...\n";
+    espera_tecla();
+  }
 
-  Origem.exibir_origem();
-  S.exibir();
+  if (Term.formatado())
+  {
+    Term.tela_alternativa();
+    Term.clrscr();
 
+    Term.modo_linhas();
+    Term.cores(CONSOLE_BRIGHT_YELLOW, CONSOLE_DEFAULT);
+    cout << " lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk \n";
+    cout << " x                                                   x \n";
+    cout << " mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj \n";
+    Term.modo_texto();
+    Term.gotoYX(2,3);
+    Term.cores(CONSOLE_BRIGHT_BLACK, CONSOLE_BRIGHT_YELLOW);
+    cout << "                      SUDOKU                       ";
+    Term.cores(CONSOLE_BRIGHT_WHITE, CONSOLE_DEFAULT);
+    Term.gotoYX(4,7);
+    cout << "TABULEIRO ATUAL";
+    Term.gotoYX(4,34);
+    cout << "TABULEIRO INICIAL";
+    Term.cores(CONSOLE_DEFAULT, CONSOLE_DEFAULT);
+
+    desenha_moldura(true);
+    desenha_moldura();
+  }
+  else
+  {
+    cout << " +-----------------------+\n";
+    cout << " |         SUDOKU        |\n";
+    cout << " +-----------------------+\n";
+  }
+
+  Origem.imprimir(true);
+
+  S = Origem;
   do
   {
+    S.imprimir();
     do
     {
       J.ler();
-    } while(!J.jogada() && !J.apagamento() &&
-            !J.resolver_jogo() && !J.preencher_jogo() &&
-            !J.novo() && !J.voltar() &&
-            !J.fim_de_jogo());
-    if (J.jogada())
-    {
-      S.fazer_jogada(J);
-    }
-    if (J.apagamento())
-    {
-      S.apagar_jogada(J,Origem);
-    }
-    if (J.preencher_jogo())
-    {
-      S.exibir_origem();
-      int N = S.resolver_casas_faceis();
-      S.exibir();
-      exibir_preenchidas(N);
-      Origem.exibir_origem();
-    }
+    } while(!J.resolver_jogo() && !J.fim_de_jogo() && !S.jogada_valida(J));
     if (J.resolver_jogo())
     {
-      S.exibir_origem();
+      S.imprimir(true);
       S.resolver();
-      exibir_resposta();
-      Origem.exibir_origem();
+      Origem.imprimir(true);
     }
-    if (J.novo())
+    else
     {
-      Origem.gerar();
-      S=Origem;
-      Origem.exibir_origem();
+      if (!J.fim_de_jogo()) S.fazer_jogada(J);
     }
-    if (J.voltar())
-    {
-      S = Origem;
-    }
-    S.exibir();
-    if (!J.fim_de_jogo() && S.fim_de_jogo())
-    {
-      exibir_congratulacoes();
-    }
-  } while(!J.fim_de_jogo());
+  } while(!J.fim_de_jogo() && !S.fim_de_jogo());
 
-  encerra_exibicao();
+  if (S.fim_de_jogo())
+  {
+    Term.gotoYX(20,16);
+    Term.limpar_linha();
+    Term.cores(CONSOLE_BRIGHT_GREEN, CONSOLE_DEFAULT);
+    cout << "PARABENS! JOGO CONCLUIDO!\n";
+    Term.cores(CONSOLE_DEFAULT, CONSOLE_DEFAULT);
+    espera_tecla();
+  }
+  Term.tela_normal();
 }
